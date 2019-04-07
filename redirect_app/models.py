b0846a7f0a4d5826d8ae2ba761a3fa36e79fd5f5
markov <i.markov@group-ib.com>
2019-04-08 00:26:56 +0300

    add models

--- redirect_app/models.py
+++ redirect_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Webmaster(models.Model):
+    name = models.CharField(max_length=255)
+    url =  models.CharField(max_length=2083)
+
+class Offer(models.Model):
+    name = models.CharField(max_length=255)
+    url =  models.CharField(max_length=2083)
+
+class Click(models.Model):
+    webmaster = models.ForeignKey('Webmaster', on_delete=models.CASCADE)
+    offer = models.ForeignKey('Offer', on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    client_user_agent = models.CharField(max_length=255)
+    client_ip = models.CharField(max_length=255)
+    client_country = models.CharField(max_length=255)
+    client_timezone = models.CharField(max_length=255)
+    client_city = models.CharField(max_length=255)
